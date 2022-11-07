7e27e55c3cc4fadf2ae26a4ac53a12ec54c6b333
HironoBaba <h-baba@alterbooth.com>
2022-11-07 22:53:08 +0900

    [add] add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
+import { Navbar, Container } from 'react-bootstrap'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            Hello EC
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
